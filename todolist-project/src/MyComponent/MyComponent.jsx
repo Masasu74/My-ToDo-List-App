8119adbeeca78b3eb4d623eea674464f14454860
Salomon Masasu <masasusalomon@gmail.com>
2024-01-14 23:07:33 +0200

    ss

--- todolist-project/src/MyComponent/MyComponent.jsx
+++ todolist-project/src/MyComponent/MyComponent.jsx
@@ -1,4 +1,5 @@
 import styles from "./MyComponent.module.css";
+import image from "../assets/trash.png";
 function MyComponent() {
   return (
     <div className={styles.container}>
@@ -7,12 +8,61 @@ function MyComponent() {
           <h1 className={styles.title}>Todo App</h1>
         </div>
         <div className={styles.dateBox}>
-          <h4 className={styles.day}>Sunday</h4>
-          <h6 className={styles.date}>January 14,2024</h6>
+          <h4 className={styles.day}>Monday</h4>
+          <h6 className={styles.date}>January 15,2024</h6>
         </div>
         <div className={styles.inputBox}>
-          <input className={styles.input} type="text" name="" id="" placeholder="Add Task" />
+          <input
+            className={styles.input}
+            type="text"
+            name=""
+            id=""
+            placeholder="Add Task"
+          />
           <button className={styles.button}>Add</button>
+        </div>
+        <div className={styles.taskBox}>
+          <ul>
+            <li>
+              <div className={styles.task}>
+                <div>Coding</div>
+                <div className={styles.trashBox}>
+                  <img src={image} width={30} alt="" />
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div className={styles.task}>
+                <div>Coding</div>
+                <div className={styles.trashBox}>
+                  <img src={image} width={30} alt="" />
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div className={styles.task}>
+                <div>Coding</div>
+                <div className={styles.trashBox}>
+                  <img src={image} width={30} alt="" />
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div className={styles.task}>
+                <div>Coding</div>
+                <div className={styles.trashBox}>
+                  <img src={image} width={30} alt="" />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.lastBox}>
+            <h5>You have 4 pending tasks</h5>
+            <button className={styles.clearBtn}>Clear</button>
         </div>
       </div>
     </div>
